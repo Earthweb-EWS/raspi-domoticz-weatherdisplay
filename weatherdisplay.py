056480ef6530769f59b4bcbe1f7160c321ddd3d9
Nathan van Dael <nathan@vandael.nu>
2018-05-18 20:22:56 +0200

    Added trend

--- weatherdisplay.py
+++ weatherdisplay.py
@@ -11,6 +11,8 @@ import time
 import subprocess
 import thread
 
+import locale
+
 from datetime import datetime
 from decimal import Decimal
 
@@ -23,6 +25,8 @@ import urllib, json
 import logging
 import os
 
+# Set to users preferred locale:
+locale.setlocale(locale.LC_ALL, '')
 
 # URL to Domoticz Device
 # http://www.domoticz.com/wiki/Domoticz_API/JSON_URL%27s#Retrieve_status_of_specific_device
@@ -30,9 +34,11 @@ import os
 #
 # Don't forget to fill in Device Description because this is the header of the display.
 #
-url = "https://URL/json.htm?username=USER&password=PASS&type=devices&rid=NUMBER"
+liveurl = "https://URL/json.htm?username=USER&password=PASS&type=devices&rid=NUMBER"
+dayurl = "https://URL/json.htm?username=USER&password=PASS&type=graph&sensor=temp&idx=NUMBER&range=day"
 
-data = 0 # Placeholder for remote JSON file content.
+livedata = 0 # Placeholder for remote JSON file content.
+daydata = 0 # Placeholder for remote JSON file content.
 mode = 1 # Default Displaymode (1-3).
 timer = 240 # x in seconds for refreshing data.
 logPath = os.getcwd() # Path to logfile.
@@ -54,11 +60,11 @@ rootLogger.addHandler(consoleHandler)
 
 
 # Get Remote Data from Domoticz.
-def getremotedata():
+def getlivedata():
 	try:
-		global url
-		if urllib.urlopen(url).getcode() == 200:
-			response = urllib.urlopen(url)
+		global liveurl
+		if urllib.urlopen(liveurl).getcode() == 200:
+			response = urllib.urlopen(liveurl)
 			return json.loads(response.read())
 		else:
 			logging.ERROR("URL kan niet worden geopend.")
@@ -66,6 +72,19 @@ def getremotedata():
 		logging.exception(e)
 		return
 
+# Get Remote Data from Domoticz.
+def getdaydata():
+	try:
+		global dayurl
+		if urllib.urlopen(dayurl).getcode() == 200:
+			response = urllib.urlopen(dayurl)
+			return json.loads(response.read())
+		else:
+			logging.ERROR("URL kan niet worden geopend.")
+	except Exception as e:
+		logging.exception(e)
+		return
+		
 def getdesc(obj):
 	desc = (obj['result'][0]['Description'])
 	if not desc:
@@ -113,13 +132,35 @@ def getsunset(obj):
 def getstatus(obj):
 	return(obj['status'])
 
-def getservertime(obj):
+def getservertime():
 	dt = datetime.strptime(str(datetime.now()), "%Y-%m-%d %H:%M:%S.%f")
 	return(dt.strftime("%d-%m-%Y %H:%M"))
 
 def getlastupdate(obj):
 	dt = datetime.strptime((obj['result'][0]['LastUpdate']), "%Y-%m-%d %H:%M:%S")
 	return(dt.strftime("%d-%m-%Y %H:%M"))
+
+def getavetemplasthour(obj):
+	value = 0
+	for x in range(len(obj['result']) - 12, len(obj['result'])):
+		value = (value + (obj['result'][x]['te']))
+	value = (value/12)
+	return(value)
+
+def getavetemp2lasthour(obj):
+	value = 0
+	for x in range(len(obj['result']) - 25, len(obj['result']) - 13):
+		value = (value + (obj['result'][x]['te']))
+	value = (value/12)
+	return(value)
+
+def gettrend():
+	value = 1
+	if getavetemplasthour(daydata) > getavetemp2lasthour(daydata):
+		value = 2
+	if getavetemplasthour(daydata) < getavetemp2lasthour(daydata):
+		value = 0
+	return value
 
 def make_font(name, size):
     font_path = os.path.abspath(os.path.join(
@@ -132,14 +173,16 @@ def starttimer(threadName, delay):
 		while True:
 			#print ("%s" % (threadName))
 			global data
-			data=getremotedata()
+			livedata=getlivedata()
+			daydata=getdaydata()
 			time.sleep(delay)
 	except Exception as e:
 		logging.exception(e)
 
 # Get first time data before the timer kicks in.
 try:
-	data=getremotedata()
+	livedata=getlivedata()
+	daydata=getdaydata()
 except Exception as e:
 	logging.exception(e)
 
@@ -151,10 +194,10 @@ except Exception as e:
 	logging.exception(e)
   
 # Print Sensor Status.
-if not data:
+if not livedata or not daydata:
 	print("{0}: Fout met ophalen data!".format(defaultDescription))
 else:
-	print("{0}: {1}".format(getdesc(data), getstatus(data)))
+	print("{0}: {1}".format(getdesc(livedata), getstatus(livedata)))
 
 
 # Load default font.
@@ -241,31 +284,38 @@ try:
 
 
 			# Write text on screen.
-			if not data:
+			if not livedata or not daydata:
 				draw.text((x, top),       defaultDescription,  font=font, fill=255)
 				draw.text((x, top+16),    "Fout met ophalen data!", font=font, fill=255)
 			else:
 				if mode == 2:
-					draw.text((x, top),       str(getdesc(data)),  font=font, fill=255)
-					draw.text((x, top+16),    "Temperatuur: " + str(gettemp(data)) + u"\u00b0C", font=font, fill=255)
-					draw.text((x, top+24),    "Dauwpunt: " + str(getdew(data)) + u"\u00b0C",  font=font, fill=255)
-					draw.text((x, top+32),    "Luchtvochtigheid: " + str(gethum(data)) + "%",  font=font, fill=255)
-					draw.text((x, top+40),    "Weertype: " + str(gethumstat(data)),  font=font, fill=255)
+					draw.text((x, top),       str(getdesc(livedata)),  font=font, fill=255)
+					draw.text((x, top+16),    "Temperatuur: " + str(gettemp(livedata)) + u"\u00b0C", font=font, fill=255)
+					draw.text((x, top+24),    "Dauwpunt: " + str(getdew(livedata)) + u"\u00b0C",  font=font, fill=255)
+					draw.text((x, top+32),    "Luchtvochtigheid: " + str(gethum(livedata)) + "%",  font=font, fill=255)
+					draw.text((x, top+40),    "Weertype: " + str(gethumstat(livedata)),  font=font, fill=255)
 				elif mode == 3:
-					draw.text((x, top),       str(getdesc(data)),  font=font, fill=255)
-					draw.text((x, top+16),    "Zonsopkomst: " + str(getsunrise(data)), font=font, fill=255)
-					draw.text((x, top+24),    "Zonsondergang: " + str(getsunset(data)),  font=font, fill=255)
+					draw.text((x, top),       str(getdesc(livedata)),  font=font, fill=255)
+					draw.text((x, top+16),    "Zonsopkomst: " + str(getsunrise(livedata)), font=font, fill=255)
+					draw.text((x, top+24),    "Zonsondergang: " + str(getsunset(livedata)),  font=font, fill=255)
 					draw.text((x, top+40),    "Bijgewerkt op: ",  font=font, fill=255)
-					draw.text((x, top+48),    "" + str(getlastupdate(data)),  font=font, fill=255)
+					draw.text((x, top+48),    "" + str(getlastupdate(livedata)),  font=font, fill=255)
 				elif mode == 8:
-					draw.text((x, top),       str(getdesc(data)),  font=font, fill=255)
-					draw.text((x, top+16),    "Status: " + str(getstatus(data)), font=font, fill=255)
-					draw.text((x, top+24),    "Batterij: " + str(getbatt(data)) + "%",  font=font, fill=255)
+					draw.text((x, top),       str(getdesc(livedata)),  font=font, fill=255)
+					draw.text((x, top+16),    "Status: " + str(getstatus(livedata)), font=font, fill=255)
+					draw.text((x, top+24),    "Batterij: " + str(getbatt(livedata)) + "%",  font=font, fill=255)
 					draw.text((x, top+40),    "Huidige datum & tijd: ",  font=font, fill=255)
-					draw.text((x, top+48),    "" + str(getservertime(data)),  font=font, fill=255)
+					draw.text((x, top+48),    "" + str(getservertime()),  font=font, fill=255)
 				else:
-					draw.text((x, top),       str(getdesc(data)),  font=font, fill=255)
-					draw.text((x, top+16),    str(gettemp(data)) + u"\u00b0C", font=fontbig, fill=255)
+					draw.text((x, top),       str(getdesc(livedata)),  font=font, fill=255)
+					draw.text((x, top+16),    str(gettemp(livedata)) + u"\u00b0C", font=fontbig, fill=255)
+
+					if gettrend() == 2: # Temperature rising
+						draw.text((x+110, top+16),       u"\u2191",  font=fontbig, fill=255)
+					elif gettrend() == 0: # Temperature falling
+						draw.text((x+110, top+16),       u"\u2193",  font=fontbig, fill=255)
+					else: # Temperature is the same
+						draw.text((x+110, top+16),       "=",  font=fontbig, fill=255)
 
 except Exception as e:
 	logging.exception(e)
